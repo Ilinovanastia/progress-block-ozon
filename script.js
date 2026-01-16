f362da43375c75d6976857d923fe37f364b9e82b
Ilinovanastia <134706795+Ilinovanastia@users.noreply.github.com>
2026-01-16 20:52:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+class ProgressAPI {
+    constructor(options = {}) {
+        this.svg = options.svg;
+        this.progressBar = options.progressBar;
+        this.container = options.container;
+        if (this.progressBar) {
+            this.radius = 45;
+            this.circumference = 2 * Math.PI * this.radius;
+            this.progressBar.style.strokeDasharray = this.circumference;
+            this.progressBar.style.strokeDashoffset = this.circumference;
+        }
+    }
+    
+    setValue(value) {
+        if (!this.progressBar) return this;
+        const val = Math.max(0, Math.min(100, value));
+        const offset = this.circumference - (val / 100) * this.circumference;
+        this.progressBar.style.strokeDashoffset = offset;
+        return this;
+    }
+    
+    setAnimate(state) {
+        if (this.svg) {
+            this.svg.classList.toggle('animated', state);
+        }
+        return this;
+    }
+    
+    setHidden(state) {
+        if (this.container) {
+            this.svg.classList.toggle('hidden', state);
+        }
+        return this;
+    }
+}
